aec6094edec5a140895e7289bd89956ec3474d6e
yanyi_work <yanyi@9399@outlook.com>
2017-12-11 18:10:43 +0800

    work

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
+
 const fs = require('fs');
 const path = require('path');
 const config = require('./src/config/defaultConfig');
 
 const dirpath = path.join(config.root,'./testHTML');
-
-fs.readdir(dirpath,(err,files) => {
-	if(err) throw err;
-	for(let i=0;i<files.length;i++){
-		const filepath = dirpath +'\\'+files[i];
-		const stats = fs.stat(filepath,(err,stats) => {
-			if(err) throw err;
-			if(stats.isFile() && files[i].indexOf('.html')){
-				const htmlfile = files[i];
-				let htmlreadstream = fs.readFileSync(dirpath+'\\'+htmlfile);
-				console.log(htmlreadstream.toString())
-			}
-		});
+let hrefbox = config.modifyhostname;
+const fn = function(pathname){
+	fs.readdir(pathname,(err,files) => {
+		if(err) throw err;
+		const ancherbuf = Buffer.from('<head>');
+		for(let i=0;i<files.length;i++){
+			const filepath = path.join(pathname ,files[i]);
 		
-	}
-});
-// console.log(files);
+			
+			const stats = fs.stat(filepath,(err,stats) => {
+				if(err) throw err;
+				if(stats.isFile() && files[i].indexOf('.html') !== -1){
+					const htmlfile = files[i];
+					const htmlreaded = fs.readFileSync(path.join(pathname,htmlfile));
+					const addbuf = Buffer.from(`\n    <link rel="canonical" href="${hrefbox}${htmlfile}">`);
+					const ancherindexof = htmlreaded.indexOf(ancherbuf);
+					const start = ancherindexof + ancherbuf.length;
+					let modifybuf = [];
+
+					const reststream = fs.createReadStream(path.join(pathname,htmlfile),{start});
+
+				
+					reststream.on('data',(buf) => {
+						modifybuf = Buffer.concat([addbuf,buf]);
+						fs.createWriteStream(path.join(pathname,htmlfile),{flags: 'r+',start}).end(modifybuf,() => {
+							console.info('success!!');
+						});
+
+					});
+				}
+				if(stats.isDirectory()){
+					hrefbox = `${hrefbox}${filepath.split('\\')[filepath.split('\\').length-1]}/`;
+					console.info(hrefbox);
+					fn(filepath);
+				}
+			});
+		
+		}
+	});
+};
+fn(dirpath);
--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,5 +1,6 @@
 module.exports = {
 	root: process.cwd(),
 	hostname : '127.0.0.1',
-	port: 8888
+	port: 8888,
+	modifyhostname: 'https://www.backup-utility.com/'
 };
